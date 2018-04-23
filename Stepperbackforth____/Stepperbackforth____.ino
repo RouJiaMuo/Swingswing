18b6993712aee2fc90d88799dc2e5dbbf2e9e51e
RouJiaMuo <31493579+RouJiaMuo@users.noreply.github.com>
2018-04-23 13:16:18 -0500

    update
    update

--- Stepperbackforth____/Stepperbackforth____.ino
+++ Stepperbackforth____/Stepperbackforth____.ino
@@ -1,4 +1,8 @@
 int Distance = 0;  
+int digit1 = 630;
+void setup() {                
+  pinMode(8, OUTPUT);     
+  pinMode(9, OUTPUT);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
 }
@@ -9,9 +13,9 @@ void loop() {
   delayMicroseconds(200);          
   digitalWrite(9, LOW); 
   delayMicroseconds(200);
-  Distance = Distance + 1; 
+  Distance = Distance + 1;   
   
-  
+  // Check to see if we are at the end of our move
   if (Distance == 1250)
   {
 
@@ -26,8 +30,8 @@ void loop() {
 
     Distance = 0;
     
-    digit1 = digit1 - 0.3;//reduce each timing by 0.3ms 
-    delay(digit1); //How can I loop line 34 and 35 when digit1 hits 0?
-    //Idealy, I want to make the digit1 pause for a random time when it hits 0, and go back to 630, and pause another random time, and then reduce again.   
+    digit1 = digit1 - 0.3;//Reduce the timing by 0.3ms
+    delay(digit1);//How can I loop line 34 and 35 when digit1 hits 0?
+     //Idealy, I want to make the digit1 pause for a random time when it hits 0, and go back to 630, and pause another random time, and then reduce again.  
   }
 }
